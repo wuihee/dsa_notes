d699e2be9a20ac1c99fa5474a1ae21238b1e56a6
wuihee <wuihee@gmail.com>
2023-10-16 12:52:28 -0700

    Updated Java DynamicArray

--- dsa_notes/data_structures/arrays/DynamicArray.java
+++ dsa_notes/data_structures/arrays/DynamicArray.java
@@ -1,3 +1,47 @@
-public class Array {
-    public Array() {}
+import java.util.*;
+
+public class DynamicArray<T> {
+    private Object[] elements;
+    private int size = 0;
+    private static final int DEFAULT_CAPACITY = 10;
+
+    public DynamicArray() {
+        elements = new Object[DEFAULT_CAPACITY];
+    }
+
+    public void add(T e) {
+        if (size == elements.length) {
+            ensureCapacity();
+        }
+        elements[size++] = e;
+    }
+
+    @SuppressWarnings("unchecked")
+    public T get(int i) {
+        if (i >= size || i < 0) {
+            throw new IndexOutOfBoundsException("Index: " + i + ", Size " + i);
+        }
+        return (T) elements[i];
+    }
+    
+    @SuppressWarnings("unchecked")
+    public T remove(int i) {
+        if (i >= size || i < 0) {
+            throw new IndexOutOfBoundsException("Index: " + i + ", Size " + i);
+        }
+        Object item = elements[i];
+        int numElts = elements.length - (i + 1);
+        System.arraycopy(elements, i + 1, elements, i, numElts);
+        size--;
+        return (T) item;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    private void ensureCapacity() {
+        int newSize = elements.length * 2;
+        elements = Arrays.copyOf(elements, newSize);
+    }
 }
