0f20c1478fcb48e231d835c6ff5be5585109d810
wuihee <wuihee@gmail.com>
2023-10-27 21:17:51 -0700

    Updated dynamic array

--- dsa_notes/data_structures/arrays/dynamic_array.py
+++ dsa_notes/data_structures/arrays/dynamic_array.py
@@ -1,3 +1,25 @@
 class DynamicArray:
-    def __init__(self):
-        pass
+    """
+    Python 'implementation' of a dynamic array. We pretend to use an immutable
+    array.
+    """
+
+    def __init__(self, array=[]) -> None:
+        """
+        Initialize the DynamicArray.
+
+        Args:
+            array (list, optional): List of elements to initialize the array
+                                    with. Defaults to [].
+        """
+        self.array = array
+        self.size = len(array)
+
+    def size(self) -> int:
+        """
+        Return the size of the array.
+
+        Returns:
+            int: The number of elements in the array.
+        """
+        return self.size
