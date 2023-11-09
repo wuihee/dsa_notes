0d7d89242f19504f916a661bd9f272e31fa6e775
wuihee <wuihee@gmail.com>
2023-11-08 19:28:27 -0800

    Updated python array

--- dsa_notes/data_structures/arrays/dynamic_array.py
+++ dsa_notes/data_structures/arrays/dynamic_array.py
@@ -1,4 +1,9 @@
-class DynamicArray:
+from typing import Generic, TypeVar
+
+T = TypeVar("T")
+
+
+class DynamicArray(Generic[T]):
     """
     Python 'implementation' of a dynamic array. We pretend to use an immutable
     array.
@@ -12,8 +17,9 @@ class DynamicArray:
             array (list, optional): List of elements to initialize the array
                                     with. Defaults to [].
         """
-        self.array = array
-        self.size = len(array)
+        self._array = array
+        self._size = len(array)
+        self._capacity = self._size
 
     def size(self) -> int:
         """
@@ -22,4 +28,30 @@ class DynamicArray:
         Returns:
             int: The number of elements in the array.
         """
-        return self.size
+        return self._size
+
+    def is_empty(self) -> bool:
+        """
+        Check if array is empty.
+
+        Returns:
+            bool: Return True if empty else False.
+        """
+        return self._size == 0
+
+    def replace(self, i: int, value: T) -> None:
+        """
+        Replace element at index i with a new value.
+
+        Args:
+            i (int): Index to replace at.
+            value (T): New value to replace with.
+        """
+        self._array[i] = value
+
+    def clear(self) -> None:
+        """
+        Empty the array.
+        """
+        self._array = [None for _ in range(self._size)]
+        self._size = 0
