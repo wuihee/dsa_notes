7bf98d851217178b3e4a4126ad01634c77ed53d9
wuihee <wuihee@gmail.com>
2023-11-08 19:46:20 -0800

    Updated dynamic array

--- dsa_notes/data_structures/ArrayListP/dynamic_array.py
+++ dsa_notes/data_structures/ArrayListP/dynamic_array.py
@@ -55,3 +55,41 @@ class DynamicArray(Generic[T]):
         """
         self._array = [None for _ in range(self._size)]
         self._size = 0
+
+    def append(self, value: T) -> None:
+        """
+        Append a value to the end of the array. If it's at its maximum
+        capacity, extend it.
+
+        Args:
+            value (T): The value to add the array.
+        """
+        if self._size + 1 > self._capacity:
+            new_capacity = self._capacity * 2 if self._capacity > 0 else 1
+            new_array = [None for _ in range(new_capacity)]
+
+            for i in range(len(self._array)):
+                new_array[i] = self._array[i]
+            self._array = new_array
+
+        self._array[self._size] = value
+        self._size += 1
+
+    def remove_at(self, i: int) -> None:
+        """
+        Remove element at index i.
+
+        Args:
+            i (int): The index at which to remove the element from.
+
+        Raises:
+            IndexError: If i is not within the array.
+        """
+        if i < 0 or i > self._size - 1:
+            raise IndexError
+
+        for j in range(i, self._size - 1):
+            self._array[j] = self._array[j + 1]
+        self._array[j] = None
+
+        self._size -= 1
